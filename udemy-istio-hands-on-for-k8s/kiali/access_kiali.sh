102afd429e3f98ece4ff3c31d3371110ac47a050
paragkalra <paragkalra@gmail.com>
2026-06-03 08:32:03 -0700

    Added command to access Kiali via minikube

---
+++ udemy-istio-hands-on-for-k8s/kiali/access_kiali.sh
@@ -0,0 +1,3 @@
+# To access Kiali
+minikube service kiali -n istio-system
+# Visit the IP and Port displayed under tunneling section. E.G:  http://127.0.0.1:51803
--- udemy-istio-hands-on-for-k8s/kiali/kiali_setup.sh
+++ udemy-istio-hands-on-for-k8s/kiali/kiali_setup.sh
@@ -5,3 +5,7 @@ kubectl apply -f 2-istio-minikube.yaml
 kubectl apply -f 3-kiali-secret.yaml
 kubectl apply -f 4-label-default-namespace.yaml
 kubectl apply -f 5-application-no-istio.yaml
+
+# To access Kiali
+minikube service kiali -n istio-system
+# Visit the IP and Port displayed under tunneling section. E.G:  http://127.0.0.1:51803
